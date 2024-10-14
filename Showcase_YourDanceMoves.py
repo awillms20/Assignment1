af785f891b0cdf40237e3af8b071e478eaafb9cc
awillms20 <awillms17@gmail.com>
2024-10-14 17:37:18 -0400

    question1

---
+++ Showcase_YourDanceMoves.py
@@ -0,0 +1,15 @@
+# Task 1: Code Correction
+weather="sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella")
+
+#Task 2: Your Mood Today
+user_input=input("How do you feel today")
+
+if "Happy":
+    print("That's great to hear")
+if "sad":
+    print("I hope your day gets better!")
