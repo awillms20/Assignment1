bbc528ec19de3adbb6332888abcfe884925241f3
awillms20 <awillms17@gmail.com>
2024-10-14 18:36:19 -0400

    question3

---
+++ Arithmetic_Operations.py
@@ -0,0 +1,16 @@
+# Task1: Grocery Store Math
+cost_of_bread=2.50
+
+cost_of_almondmilk=4.58
+
+cost_of_water=12.78
+
+Total_cost= cost_of_bread+cost_of_almondmilk+cost_of_water
+print("Your total is", Total_cost)
+
+#Task2: Bank Intrest
+
+initial_amount=10000
+intrest_rate=0.07
+total_amount= initial_amount*intrest_rate
+print(total_amount)
